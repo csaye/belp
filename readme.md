ff921478b50ba515817927f3331ed4d1fc5134ec
Cooper Saye <coopersaye@gmail.com>
2020-10-16 08:37:35 -0700

    Rename project.

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# Ravenous
+# Belp
 
-A yelp clone for restaurants.
+A Yelp clone.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
     this.searchYelp = this.searchYelp.bind(this);
   }
   searchYelp(term, location, sortBy) {
+    if (!term || !location) {
+      alert('Please enter a term and a location.');
+      return;
+    }
     Yelp.search(term, location, sortBy).then(businesses => {
       this.setState({
         businesses: businesses
@@ -22,7 +26,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>belp</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
